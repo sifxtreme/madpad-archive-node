8dfd89e61e04c58e33390de9325922a41ac66726
Asif Ahmed <asifahmed2011@gmail.com>
2013-09-23 21:21:39 -0700

    add application files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 madpad-node
 ===========
 
-Create an editable pad similar to madpad but this time with Node / Express
+Create an editable pad similar to (Madpad)[https://github.com/sifxtreme/madpad] but this time with Node / Express
--- web.js
+++ web.js
@@ -1,15 +1,20 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.logger());
+
+var hbs = require('hbs');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+app.use(express.bodyParser());
+
+app.use(express.static('public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index');
 });
 app.get('/:id', function(request, response){
-	response.send('Not Hello' + request.params.id);
+	response.render('pad',{id: request.params.id});
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
